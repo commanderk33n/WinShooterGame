3ba49d0f54000bf93f58991c0d34b424f8e95c87
eikood <hanfaplast@googlemail.com>
2016-10-29 18:42:16 +0200

    added Mouse/Keyboard/Touch controls, battleship is now moveable

--- WinShooterGame/Game1.cs
+++ WinShooterGame/Game1.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 using Shooter;
 
 namespace WinShooterGame
@@ -9,10 +11,29 @@ namespace WinShooterGame
     /// </summary>
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
         // Represents the player
-        Player player;
+        private Player player;
+
+        // Keyboard states used to determine key presses
+        private KeyboardState currentKeyboardState;
+
+        private KeyboardState previousKeyboardState;
+
+        // Gamepad states used to determine button presses
+        private GamePadState currentGamePadState;
+
+        private GamePadState previousGamePadState;
+
+        // Mouse states use to track Mouse button press
+        private MouseState currentMouseState;
+
+        private MouseState previousMouseState;
+
+        // Movement speed for the player
+        private float playerMoveSpeed;
 
         public Game1()
         {
@@ -30,6 +51,10 @@ namespace WinShooterGame
         {
             // Initialize the player class
             player = new Player();
+            // Set a constant player move speed
+            playerMoveSpeed = 8.0f;
+            //Enable the FreeDrag gesture.
+            TouchPanel.EnabledGestures = GestureType.FreeDrag;
             base.Initialize();
         }
 
@@ -63,9 +88,61 @@ namespace WinShooterGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
+            // Save the previous state of the keyboard and game pad so we can determine single key/button presses
+
+            previousGamePadState = currentGamePadState;
+
+            previousKeyboardState = currentKeyboardState;
+
+            // Read the current state of the keyboard and gamepad and store it
+
+            currentKeyboardState = Keyboard.GetState();
+
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+            //Update the player
+
+            UpdatePlayer(gameTime);
 
             base.Update(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+            // Get Thumbstick Controls
+
+            player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+            player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+            // Use the Keyboard / Dpad
+
+            if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+
+            {
+                player.Position.X -= playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+
+            {
+                player.Position.X += playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+
+            {
+                player.Position.Y -= playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+
+            {
+                player.Position.Y += playerMoveSpeed;
+            }
+            // Make sure that the player does not go out of bounds
+
+            player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+
+            player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
         }
 
         /// <summary>
