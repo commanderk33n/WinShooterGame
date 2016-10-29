48ccd40c10c2bb492298a3a2c49658503eaa6851
eikood <hanfaplast@googlemail.com>
2016-10-29 19:48:51 +0200

    Feierabend

--- WinShooterGame/Game1.cs
+++ WinShooterGame/Game1.cs
@@ -19,19 +19,13 @@ namespace WinShooterGame
 
         // Keyboard states used to determine key presses
         private KeyboardState currentKeyboardState;
-
         private KeyboardState previousKeyboardState;
-
         // Gamepad states used to determine button presses
         private GamePadState currentGamePadState;
-
         private GamePadState previousGamePadState;
-
         // Mouse states use to track Mouse button press
         private MouseState currentMouseState;
-
         private MouseState previousMouseState;
-
         // Movement speed for the player
         private float playerMoveSpeed;
 
@@ -66,7 +60,6 @@ namespace WinShooterGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // Load the player resources
             Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
             player.Initialize(Content.Load<Texture2D>("Graphics//player"), playerPosition);
@@ -89,59 +82,40 @@ namespace WinShooterGame
         protected override void Update(GameTime gameTime)
         {
             // Save the previous state of the keyboard and game pad so we can determine single key/button presses
-
             previousGamePadState = currentGamePadState;
-
             previousKeyboardState = currentKeyboardState;
-
             // Read the current state of the keyboard and gamepad and store it
-
             currentKeyboardState = Keyboard.GetState();
-
             currentGamePadState = GamePad.GetState(PlayerIndex.One);
-
             //Update the player
-
             UpdatePlayer(gameTime);
-
             base.Update(gameTime);
         }
 
         private void UpdatePlayer(GameTime gameTime)
         {
             // Get Thumbstick Controls
-
             player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
             player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
             // Use the Keyboard / Dpad
-
             if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
-
             {
                 player.Position.X -= playerMoveSpeed;
             }
-
             if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
-
             {
                 player.Position.X += playerMoveSpeed;
             }
-
             if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
-
             {
                 player.Position.Y -= playerMoveSpeed;
             }
-
             if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
-
             {
                 player.Position.Y += playerMoveSpeed;
             }
             // Make sure that the player does not go out of bounds
-
             player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
-
             player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
         }
 
@@ -152,14 +126,12 @@ namespace WinShooterGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // Start drawing
             spriteBatch.Begin();
             // Draw the Player
             player.Draw(spriteBatch);
             // Stop drawing
             spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
